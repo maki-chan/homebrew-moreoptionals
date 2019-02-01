0b559b91d3a5f5504f05a1ade5d307838914eac6
Michael Armbruster <6783670+maki-chan@users.noreply.github.com>
2019-02-01 13:04:45 +0100

    Add davs2 (video decoder)

---
+++ Formula/davs2.rb
@@ -0,0 +1,18 @@
+class Davs2 < Formula
+  desc "An open-source decoder of AVS2-P2/IEEE1857.4 video coding standard"
+  homepage "https://github.com/pkuvcl/davs2"
+  url "https://github.com/pkuvcl/davs2/archive/1.6.tar.gz"
+  sha256 "de93800f016cbaf08cb40184a8069050dc625da5240a528155137052d1cf81c8"
+  
+  def install
+    system "./configure", "--enable-shared",
+                          "--prefix=#{prefix}"
+    
+    system "make", "install"
+  end
+
+  test do
+    # For now, I do not have a suitable idea to test the functionality
+    system "true"
+  end
+end
