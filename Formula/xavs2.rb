d4c7b662089d4f6cd68001270e68fca0e68c150b
Michael Armbruster <6783670+maki-chan@users.noreply.github.com>
2019-02-01 13:05:07 +0100

    Add xavs2 (video encoder)

---
+++ Formula/xavs2.rb
@@ -0,0 +1,19 @@
+class Xavs2 < Formula
+  desc "An open-source encoder of AVS2-P2/IEEE1857.4 video coding standard"
+  homepage "https://github.com/pkuvcl/xavs2"
+  url "https://github.com/pkuvcl/xavs2/archive/1.3.tar.gz"
+  sha256 "28f9204dc9384336de7c6210cd3317d2d6b94ec23a4d1b6113fcbe7f00d7230b"
+
+  def install
+    system "./configure", "--enable-shared",
+                          "--enable-static",
+                          "--prefix=#{prefix}"
+    
+    system "make", "install"
+  end
+
+  test do
+    # Will not test the functionality until a suitable test case is found
+    system "true"
+  end
+end
