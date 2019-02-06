a1ac2c441319ca5657c6e73044e0e0427981bbfa
Michael Armbruster <6783670+maki-chan@users.noreply.github.com>
2019-02-06 19:58:11 +0100

    Create mkvtoolnix-full
    Core repository removed option to build GUI which makes the core formula quite useless

---
+++ Formula/mkvtoolnix-full.rb
@@ -0,0 +1,72 @@
+class MkvtoolnixFull < Formula
+  desc "Matroska media files manipulation tools"
+  homepage "https://mkvtoolnix.download/"
+  url "https://mkvtoolnix.download/sources/mkvtoolnix-30.1.0.tar.xz"
+  sha256 "4628f40d62d359edb1441c52381b1f3a61aa227279133d7e01257f91e0d92591"
+
+  head do
+    url "https://gitlab.com/mbunkus/mkvtoolnix.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "docbook-xsl" => :build
+  depends_on "fmt" => :build
+  depends_on "pkg-config" => :build
+  depends_on "pugixml" => :build
+  depends_on "boost"
+  depends_on "flac"
+  depends_on "gettext"
+  depends_on "libebml"
+  depends_on "libmagic"
+  depends_on "libmatroska"
+  depends_on "libogg"
+  depends_on "libvorbis"
+  depends_on "qt"
+  depends_on "cmark"
+
+  def install
+    ENV.cxx11
+
+    features = %w[flac libebml libmagic libmatroska libogg libvorbis]
+    extra_includes = ""
+    extra_libs = ""
+    features.each do |feature|
+      extra_includes << "#{Formula[feature].opt_include};"
+      extra_libs << "#{Formula[feature].opt_lib};"
+    end
+    extra_includes.chop!
+    extra_libs.chop!
+    
+    qt = Formula["qt"]
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-boost=#{Formula["boost"].opt_prefix}",
+                          "--with-docbook-xsl-root=#{Formula["docbook-xsl"].opt_prefix}/docbook-xsl",
+                          "--with-extra-includes=#{extra_includes}",
+                          "--with-extra-libs=#{extra_libs}",
+                          "--with-moc=#{qt.opt_bin}/moc",
+                          "--with-uic=#{qt.opt_bin}/uic",
+                          "--with-rcc=#{qt.opt_bin}/rcc",
+                          "--enable-qt"
+    system "rake", "-j#{ENV.make_jobs}"
+    system "rake", "install"
+  end
+
+  test do
+    mkv_path = testpath/"Great.Movie.mkv"
+    sub_path = testpath/"subtitles.srt"
+    sub_path.write <<~EOS
+      1
+      00:00:10,500 --> 00:00:13,000
+      Homebrew
+    EOS
+
+    system "#{bin}/mkvmerge", "-o", mkv_path, sub_path
+    system "#{bin}/mkvinfo", mkv_path
+    system "#{bin}/mkvextract", "tracks", mkv_path, "0:#{sub_path}"
+  end
+end
