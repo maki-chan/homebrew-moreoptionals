f377986777c986fcd10ee4649079fe54c55d55e7
Michael Armbruster <6783670+maki-chan@users.noreply.github.com>
2019-11-05 08:46:14 +0100

    Update ffmpeg-full revision: updated dependencies

--- Formula/ffmpeg-full.rb
+++ Formula/ffmpeg-full.rb
@@ -2,7 +2,7 @@ class FfmpegFull < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.2.1.tar.xz"
-  revision 1
+  revision 2
   sha256 "cec7c87e9b60d174509e263ac4011b522385fd0775292e1670ecc1180c9bb6d4"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
