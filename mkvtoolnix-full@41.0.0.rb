682bc1e2d73cd427d5002e799cdd7f4839d515df
Michael Armbruster <6783670+maki-chan@users.noreply.github.com>
2020-03-20 22:00:43 +0100

    Create mkvtoolnix-full@41.0.0.rb for older macOS

---
+++ mkvtoolnix-full@41.0.0.rb
@@ -0,0 +1,68 @@
+class MkvtoolnixFullAT4100 < Formula
+  desc "Matroska media files manipulation tools"
+  homepage "https://mkvtoolnix.download/"
+  url "https://mkvtoolnix.download/sources/mkvtoolnix-41.0.0.tar.xz"
+  sha256 "7cdd6ad9144324162604172b7e96cb13cdbb7913e07c4ad7633344aac20e4103"
+
+  depends_on "docbook-xsl" => :build
+  depends_on "fmt" => :build
+  depends_on "pkg-config" => :build
+  depends_on "pugixml" => :build
+  depends_on "boost"
+  depends_on "flac"
+  depends_on "gettext"
+  depends_on "libebml"
+  depends_on "libmagic"
+  depends_on "libmatroska"
+  depends_on "libogg"
+  depends_on "libvorbis"
+  depends_on "qt"
+  depends_on "cmark"
+
+  uses_from_macos "libxslt" => :build
+  uses_from_macos "ruby" => :build
+
+  def install
+    ENV.cxx11
+
+    features = %w[flac libebml libmagic libmatroska libogg libvorbis]
+    extra_includes = ""
+    extra_libs = ""
+    features.each do |feature|
+      extra_includes << "#{Formula[feature].opt_include};"
+      extra_libs << "#{Formula[feature].opt_lib};"
+    end
+    extra_includes.chop!
+    extra_libs.chop!
+    
+    qt = Formula["qt"]
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-boost=#{Formula["boost"].opt_prefix}",
+                          "--with-docbook-xsl-root=#{Formula["docbook-xsl"].opt_prefix}/docbook-xsl",
+                          "--with-extra-includes=#{extra_includes}",
+                          "--with-extra-libs=#{extra_libs}",
+                          "--with-moc=#{qt.opt_bin}/moc",
+                          "--with-uic=#{qt.opt_bin}/uic",
+                          "--with-rcc=#{qt.opt_bin}/rcc",
+                          "--enable-qt"
+    system "rake", "-j#{ENV.make_jobs}"
+    system "rake", "install"
+  end
+
+  test do
+    mkv_path = testpath/"Great.Movie.mkv"
+    sub_path = testpath/"subtitles.srt"
+    sub_path.write <<~EOS
+      1
+      00:00:10,500 --> 00:00:13,000
+      Homebrew
+    EOS
+
+    system "#{bin}/mkvmerge", "-o", mkv_path, sub_path
+    system "#{bin}/mkvinfo", mkv_path
+    system "#{bin}/mkvextract", "tracks", mkv_path, "0:#{sub_path}"
+  end
+end
